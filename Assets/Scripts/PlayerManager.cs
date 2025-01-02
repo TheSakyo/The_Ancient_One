7a256bee76932481778fb9636d3512e4f0518bd5
Sacha Crd <sacha.cardone@gmail.com>
2025-01-02 16:25:24 +0100

    Mise à jour du 'PlayerManager.cs' + commentaire des fonctions

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,81 +1,93 @@
 using UnityEngine;
 
+/// <summary>
+/// Classe permettant de gérer tout ce qui est lié au joueur.
+/// </summary>
 public class PlayerManager : MonoBehaviour {
 
     /**********************************************/
     /***               PROPRIÉTÉS               ***/
     /**********************************************/
 
+    /// <summary>
+    /// Vitesse de mouvement du joueur
+    /// </summary>
     [SerializeField]
-    private float moveSpeed;         // Vitesse de mouvement
+    private float moveSpeed;
 
+    /// <summary>
+    /// Force du saut du joueur
+    /// </summary>
     [SerializeField]
-    private float jumpForce;        // Force du saut
+    private float jumpForce;
 
-    private bool _isGrounded;             // Permet de vérifier si le joueur est au sol
+    /// <summary>
+    /// Permet de vérifier si le joueur est au sol
+    /// </summary>
+    private bool _isGrounded;
 
-    private Rigidbody _rigideBody;        // Composant Rigidbody2D du joueur
+    /// <summary>
+    /// Composant Rigidbody2D du joueur
+    /// </summary>
+    private Rigidbody _rigideBody;
 
     /**********************************************/
     /***              CYCLE DE VIE              ***/
     /**********************************************/
 
-    /**
-     * Lors du lancement du jeu, recherche le composant Rigidbody2D du joueur
-     */
-    void Start() {
+    /// <summary>
+    /// Lors du lancement du jeu, recherche le composant Rigidbody2D du joueur
+    /// </summary>
+    void Start() { _rigideBody = GetComponent<Rigidbody>(); }
 
-        _rigideBody = GetComponent<Rigidbody>();
-    }
-
-    /**
-     * Lors de chaque frame, vérifie si le joueur appuie sur une touche du clavier
-     */
-    void Update()
-    {
-
-        // Vérifie si le joueur appuie sur une touche du clavier
-        float moveInput = Input.GetAxis("Horizontal");
-
-        // Déplace le joueur
-        _rigideBody.velocity = new Vector2(moveInput * moveSpeed, _rigideBody.velocity.y);
+    /// <summary>
+    /// Lors de chaque frame, gestion du saut du joueur
+    /// </summary>
+    void Update() {
 
         // Gestion du saut du joueur
         if(Input.GetButtonDown("Jump") && _isGrounded) {
 
             // Ajoute une force verticale au joueur
-            _rigideBody.velocity = new Vector2(_rigideBody.velocity.x, _rigideBody.velocity.y + jumpForce);
+            _rigideBody.linearVelocity = new Vector2(_rigideBody.linearVelocity.x, _rigideBody.linearVelocity.y + jumpForce);
         }
     }
 
+    /// <summary>
+    /// Lors de chaque frame corrigé, gestion du mouvement du joueur
+    /// </summary>
     void FixedUpdate()  {
 
         // Vérifie si le joueur appuie sur une touche du clavier
         float moveInput = Input.GetAxis("Horizontal");
 
-        // Déplace le joueur
-        _rigideBody.velocity = new Vector2(moveInput * moveSpeed, _rigideBody.velocity.y);
+        // Déplace le joueur avec la vélocité linéaire du Rigidbody
+        _rigideBody.linearVelocity = new Vector2(moveInput * moveSpeed, _rigideBody.linearVelocity.y);
     }
 
     /**********************************************/
     /***              ÉVÈNEMENTS                ***/
     /**********************************************/
 
-    /**
-      * Lorsqu'un objet entre en collision avec le joueur, on vérifie si le joueur est au sol
-      * pour lui changer le booléen 'isGrounded'.
-      */
+    /// <summary>
+    /// Lorsqu'un objet entre en collision avec le joueur, on vérifie si le joueur est au sol
+    /// pour lui changer le booléen 'isGrounded'.
+    /// </summary>
+    /// <param name="collision">La collision à laquelle le joueur est entré.</param>
     private void OnCollisionEnter(Collision collision) {
 
+        // Vérifie si le joueur est au sol, si c'est le cas, le booléen 'isGrounded' est vrai
         if(collision.gameObject.CompareTag("Ground")) _isGrounded = true;
     }
 
-    /**
-      * Lorsqu'un objet sort de la collision avec le joueur, on vérifie si le joueur est au sol
-      * pour lui changer le booléen 'isGrounded'.
-      */
+    /// <summary>
+    /// Lorsqu'un objet sort de la collision avec le joueur, on vérifie si le joueur est au sol
+    /// pour lui changer le booléen 'isGrounded'.
+    /// </summary>
+    /// <param name="collision">La collision à laquelle le joueur est entré.</param>
     private void OnCollisionExit(Collision collision) {
 
+        // Vérifie si le joueur est au sol, si c'est le cas, le booléen 'isGrounded' est faux
         if(collision.gameObject.CompareTag("Ground")) _isGrounded = false;
     }
 }
