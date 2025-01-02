ddd616f6b98c7073171d259ad9b6fbdf62bad32c
Sacha Crd <sacha.cardone@gmail.com>
2025-01-02 16:28:22 +0100

    Ajout de commentaire un d'un TODO sur la Class InteractorManager.cs

--- Assets/Scripts/InteractorManager.cs
+++ Assets/Scripts/InteractorManager.cs
@@ -1,32 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Classe permettant de gérer les interactions du joueur.
+/// </summary>
 public class InteractorManager : MonoBehaviour {
 
     /**********************************************/
     /***               PROPRIÉTÉS               ***/
     /**********************************************/
 
+    /// <summary>
+    /// Distance entre l'interacteur et le joueur
+    /// </summary>
     [SerializeField]
-    private float distanceToInteract;    // Distance entre l'ennemi et le joueur pour pouvoir interagir
+    private float distanceToInteract;
 
+    /// <summary>
+    /// Référence du composant permettant de récupérer le joueur cible
+    /// </summary>
     [SerializeField]
-    private Transform player;           // Référence du composant permettant de récupérer le joueur cible
+    private Transform player;
 
     /**********************************************/
     /***              CYCLE DE VIE              ***/
     /**********************************************/
 
+    /// <summary>
+    /// Lors de chaque frame, on gère si le joueur est assez proche de l'interacteur.
+    /// Si c'est le cas, on peut effectuer une intéraction avec le joueur.
+    /// </summary>
     void Update() {
 
-        // Calculate the distance between enemy's and the player
+        // Calcul de la distance entre l'interacteur et le joueur
         float distance = Vector3.Distance(transform.position, player.position);
 
-        // If the player is within follow distance, move towards the player
+        // Si la distance est suffisamment petite, on peut effectuer une intéraction
         if(distance < distanceToInteract) {
 
-
+            // TODO : Afficher le texte de l'UI lié à l'intéraction (Bulle de conversation)
         }
     }
 }
